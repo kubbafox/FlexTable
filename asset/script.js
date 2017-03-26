25c8369ac32c820f8848587edbd99dd3cc675100
Chenfeng Lai <Kubbafox@gmail.com>
2017-03-26 01:04:02 -0400

    Feature 1: drag any of the columns and position them anywhere.

--- asset/script.js
+++ asset/script.js
@@ -1,48 +1,51 @@
 /**
  * Created by Neverland on 3/26/17.
  */
-$(function () {
+$(function() {
 
     $(".column").draggable({
-        start: function (event, ui) {
+        start : function (event , ui){
             var target = document.getElementById(this.id);
-            target.style.zIndex = 100;
+            target.style.zIndex=100;
         },
-        stop: function (event, ui) {
+        stop : function (event , ui){
 
             var currentPosition = new Object();
             var newPosition = new Array();
+
             for (var i = 2; i < 8; i++) {
                 var positionData = getPosition(i);
-                nowPosition = {'name': 'column' + i, 'position': positionData};
-                newPosition.push(nowPosition);
+                currentPosition = {'name':'column'+i,'position':positionData};
+
+                newPosition.push(currentPosition);
             }
 
-            newPosition.sort(function (a, b) {
-                if (a['position'] > b['position']) return -1;
-                if (a['position'] < b['position']) return 1;
+            newPosition.sort(function(a,b){
+                if( a['position'] > b['position'] ) return -1;
+                if( a['position'] < b['position'] ) return 1;
                 return 0;
             });
 
             var number = 0;
-            for (var i = newPosition.length; i--;) {
+            for (var i = newPosition.length; i--; ) {
                 console.log(newPosition[i].name);
 
                 var tmpItem = document.getElementById(newPosition[i].name);
                 tmpItem.style.order = number;
                 tmpItem.style.left = 0;
                 tmpItem.style.top = 0;
-                number++;
+
+                number ++;
             }
 
             var target = document.getElementById(this.id);
-            target.style.zIndex = 0;
+            target.style.zIndex=0;
         }
 
     });
 
-    function getPosition(columnId) {
-        var tmpItem = document.getElementById('column' + columnId);
+    function getPosition(columnId){
+        var tmpItem = document.getElementById('column'+columnId);
         return tmpItem.offsetLeft;
     }
 });
